7813b04bdf35ce3a1a1ae3bd36d2b4e4998b747d
Ferdinand II Dangaran <160829247+Dangaran-FerdinandII-BSIT32E2@users.noreply.github.com>
2024-04-26 14:47:18 +0800

    changed smth

--- app/src/main/java/com/example/recipebook/MainActivity.kt
+++ app/src/main/java/com/example/recipebook/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    RecipeApp() //calls RecipeApp in RecipeStartScreen.kt
+                    RecipeApp() //calls RecipeApp in RecipeBook.kt
                 }
             }
         }
